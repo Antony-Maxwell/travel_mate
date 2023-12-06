1d62033766f6896fb9b0c26f69bcbd2fec1570a2
Maxwell vj <antonymaxwell619@gmail.com>
2023-12-06 10:14:04 +0530

    testing the new branch

--- lib/authenthication/signUp.dart
+++ lib/authenthication/signUp.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:travel_mate/db/functions/functions/db_functions.dart';
-import 'package:lottie/lottie.dart';
 import 'package:email_validator/email_validator.dart';
 
 class signUp extends StatefulWidget {
